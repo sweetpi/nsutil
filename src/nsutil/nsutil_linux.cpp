69d2a8cab04962b2b667e56fdddb5a9c3643ca95
capitalfuse <capitalfuse@users.noreply.github.com>
2015-10-26 09:28:08 +0900

    Update nsutil_linux.cpp

--- src/nsutil/nsutil_linux.cpp
+++ src/nsutil/nsutil_linux.cpp
@@ -11,32 +11,32 @@ using v8::String;
 
 void Init(v8::Handle<v8::Object> exports) {
 #if SUTIL_HAVE_IOPRIO
-    exports->Set(Nan::New<String>("nsutil_proc_ioprio_get"), 
+    exports->Set(Nan::New<String>("nsutil_proc_ioprio_get").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_proc_ioprio_get)->GetFunction());
-    exports->Set(Nan::New<String>("nsutil_proc_ioprio_set"), 
+    exports->Set(Nan::New<String>("nsutil_proc_ioprio_set").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_proc_ioprio_set)->GetFunction());
 #endif
 
-    exports->Set(Nan::New<String>("nsutil_disk_partitions"), 
+    exports->Set(Nan::New<String>("nsutil_disk_partitions").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_disk_partitions)->GetFunction());
 
-    exports->Set(Nan::New<String>("nsutil_sysinfo"), 
+    exports->Set(Nan::New<String>("nsutil_sysinfo").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_sysinfo)->GetFunction());
 
-    exports->Set(Nan::New<String>("nsutil_proc_cpu_affinity_get"), 
+    exports->Set(Nan::New<String>("nsutil_proc_cpu_affinity_get").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_proc_cpu_affinity_get)->GetFunction());
 
-    exports->Set(Nan::New<String>("nsutil_users"), 
+    exports->Set(Nan::New<String>("nsutil_users").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_users)->GetFunction());
 
-    exports->Set(Nan::New<String>("nsutil_pid_exists"), 
+    exports->Set(Nan::New<String>("nsutil_pid_exists").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_pid_exists)->GetFunction());
 
-    exports->Set(Nan::New<String>("nsutil_sysconf"), 
+    exports->Set(Nan::New<String>("nsutil_sysconf").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_sysconf)->GetFunction());
 
     /*
-    exports->Set(Nan::New<String>("nsutil_"), 
+    exports->Set(Nan::New<String>("nsutil_").ToLocalChecked(), 
             Nan::New<FunctionTemplate>(nsutil_)->GetFunction());
 
             */
